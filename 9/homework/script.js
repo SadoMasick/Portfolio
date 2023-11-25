bcd1db6200c6aed15db8113b1fb89cfd0bf1cb5b
SadoMasick <151945507+SadoMasick@users.noreply.github.com>
2023-11-25 15:48:54 +0300

    Update script.js

--- 9/homework/script.js
+++ 9/homework/script.js
@@ -1,15 +1,15 @@
-// function chat()
-// {
-//     alert("Hello, I like cats");
-//     let change = confirm("Do you like cats?");
-//     if (change == 1)
-//     {
-//         alert("And you?! Soooooo gooooood!");
-//     }
-//     else
-//     {
-//         alert("OK. Nice to meet you")
-//     }
-// }
+function chat()
+{
+    alert("Hello, I like cats");
+    let change = confirm("Do you like cats?");
+    if (change == 1)
+    {
+        alert("And you?! Soooooo gooooood!");
+    }
+    else
+    {
+        alert("OK. Nice to meet you")
+    }
+}
 
-// chat();
+chat();
